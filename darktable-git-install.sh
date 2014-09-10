674d61d15b9c5ffedad6ab60cb5351f4528fc290
Jiri Folta <jiri.folta@centrum.cz>
2014-09-10 19:01:07 +0200

    Update darktable-git-install.sh
    added comments and removed the older config files

--- darktable-git-install.sh
+++ darktable-git-install.sh
@@ -2,9 +2,20 @@
 
 # Download git source code, build and install it
 
-rm -rf  $HOME/darktable
+# remove the older source code
+rm -rf $HOME/darktable
+
+# remove the DT config files
+rm -rf $HOME/.config/darktable
+
 cd
+
+# cloning the current github code
 git clone git://github.com/darktable-org/darktable.git
+
+# build the code
 cd $HOME/darktable
 ./build.sh
+
+# install
 cd build && sudo make install
